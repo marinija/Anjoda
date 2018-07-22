8bf4f6f0893d7a4b9c4f3642f676252ccb32b87f
Davor <davormihajleski@gmail.com>
2018-07-22 20:52:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(function () {
+    
+    /* -------- Fixed navbar ------------*/
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $("#about-us").position();
+        if (scroll > topDist.top) {
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').removeClass('fixed-top');
+        }
+    });
+    /* Smooth scrolling */
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+    
+    /* Contact button animate scroll */
+    $('#contact-today').click(function(){
+        $("html, body").animate({
+            scrollTop: $('#contact').offset().top
+        }, 800);
+    });
+})
