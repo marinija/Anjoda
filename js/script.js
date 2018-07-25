11a02f740c9212bab82d713a4cd9d8b9df9545c5
Davor <davormihajleski@gmail.com>
2018-07-25 21:25:32 +0200

    update

--- js/script.js
+++ js/script.js
@@ -88,5 +88,28 @@ $(function () {
             
         }
     });
+    
+    /* --------------------- Back to top function ---------------------- */
+    if ($('#back-to-top').length) {
+    var scrollTrigger = 500, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+    }
 })
    
